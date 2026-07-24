a1d3cc5ea2146f6eb8d4d0b923fbe4bf03dc17d3
Michael Small <michaelsmalldev@gmail.com>
2026-07-23 23:56:48 -0500

    chore: initialized required input in test

--- client/src/app/company-card/company-card.component.spec.ts
+++ client/src/app/company-card/company-card.component.spec.ts
@@ -2,10 +2,23 @@ import { beforeEach, describe, expect, it } from 'vitest';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CompanyCardComponent } from './company-card.component';
+import { Company } from '../company-list/company';
 
 describe('CompanyCardComponent', () => {
   let component: CompanyCardComponent;
   let fixture: ComponentFixture<CompanyCardComponent>;
+  // The `setInput` for fixtures is unfortunately not typed, but
+  // this practice can make things safer. Perhaps it is overkill,
+  // or could be worth making a util type
+  const requiredInputDefaults: {
+    company: ReturnType<typeof fixture.componentInstance.company>;
+  } = {
+    company: {
+      _id: '1',
+      count: 0,
+      users: [],
+    },
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,7 +27,9 @@ describe('CompanyCardComponent', () => {
 
     fixture = TestBed.createComponent(CompanyCardComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // Required inputs must be initialized or else the test fails
+    fixture.componentRef.setInput('company', requiredInputDefaults.company);
+    await fixture.whenStable();
   });
 
   it('should create', () => {
