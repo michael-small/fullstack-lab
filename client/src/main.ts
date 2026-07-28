0103175cc1f40a2b658b31bd8cc3af6a83dac5d1
Michael Small <michaelsmalldev@gmail.com>
2026-07-27 19:57:25 -0500

    chore: remove `importProvidersFrom`
    I have never seen this in my apps I have upgraded or other client's/friend's apps. I looked at the docs, and it does not seem needed. It is just adding noise, and I use all the contained items.
    
    Anything that needs to be in providers these days in the contained modules are just done via the standalone app syntax `bootstrapApplication > providers`. The docs on that used to be not great years ago, but are solid now.

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,49 +4,13 @@ import {
   withInterceptorsFromDi,
   withXhr,
 } from '@angular/common/http';
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app/routes';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatOptionModule } from '@angular/material/core';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatTooltipModule } from '@angular/material/tooltip';
-import { BrowserModule, bootstrapApplication } from '@angular/platform-browser';
+import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
-
-const MATERIAL_MODULES = [
-  MatButtonModule,
-  MatCardModule,
-  MatDividerModule,
-  MatExpansionModule,
-  MatFormFieldModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatOptionModule,
-  MatRadioModule,
-  MatSidenavModule,
-  MatSelectModule,
-  MatSnackBarModule,
-  MatToolbarModule,
-  MatTooltipModule,
-];
 
 if (environment.production) {
   enableProdMode();
@@ -55,13 +19,6 @@ if (environment.production) {
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    importProvidersFrom(
-      BrowserModule,
-      FormsModule,
-      ReactiveFormsModule,
-      MATERIAL_MODULES,
-      LayoutModule,
-    ),
     provideAnimations(),
     provideHttpClient(withXhr(), withInterceptorsFromDi()),
   ],
